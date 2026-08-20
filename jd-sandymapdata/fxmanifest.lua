100423233fb2aae2c538f3ab7b4dec7306d615c4
JohnChinFiveM <johnr6schin@gmail.com>
2026-08-19 19:41:35 -0500

    Add files via upload

---
+++ jd-sandymapdata/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'John Chin'
+description 'Sandy Shores mapdata'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/*.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
